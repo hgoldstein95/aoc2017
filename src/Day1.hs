5d9c801ab86855dcc5a1f5eeaafa639c9ac744ea
Harrison Goldstein <hgoldstein95@gmail.com>
2017-12-14 10:30:54 -0500

    Initial commit

---
+++ src/Day1.hs
@@ -0,0 +1,25 @@
+module Day1 where
+
+import Test.HUnit
+
+type Captcha = [Integer]
+
+toCaptcha :: Integer -> Captcha
+toCaptcha = map (read . (:[])) . show
+
+solve :: Captcha -> Integer
+solve [] = 0
+solve (x : xs) = recSolve 0 $ (x : xs) ++ [x]
+  where recSolve n [] = n
+        recSolve n (_ : []) = n
+        recSolve n (y1 : y2 : ys) =
+          recSolve (if y1 == y2 then y1 + n else n) (y2 : ys)
+
+tests :: Test
+tests =
+  test
+    [ "for 1122," ~: 3 ~=? (solve [1, 1, 2, 2])
+    , "for 1111," ~: 4 ~=? (solve [1, 1, 1, 1])
+    , "for 1234," ~: 0 ~=? (solve [1, 2, 3, 4])
+    , "for 91212129," ~: 9 ~=? (solve [9, 1, 2, 1, 2, 1, 2, 9])
+    ]
